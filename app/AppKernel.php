8c60399e7ae8b1ee5be9b242913e98648a624d95
Mateusz Zalewski <zaleslaw@gmail.com>
2017-03-16 21:17:17 +0100

    Configuration fixes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
 
             new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
             new \Sylius\Bundle\ApiBundle\SyliusApiBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
 
             new \AppBundle\AppBundle(),
         ];
